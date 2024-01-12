a9155ed48375a40814489a575098f6a7852153eb
cerea62 <cerea62@yandex.ru>
2024-01-12 18:37:43 +0300

    Исправлены автотесты - auth

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -7,13 +7,13 @@ module.exports = (req, res, next) => {
   try {
     const token = req.headers.authorization;
     if (!token) {
-      throw new AccessError('Неправильные имя пользователя или пароль');
+      return next(new AccessError('Неправильные имя пользователя или пароль'));
     }
     const validToken = token.replace('Bearer ', '');
     payload = jwt.verify(validToken, SECRET_KEY);
   } catch (error) {
     if (error.name === 'JsonWebTokenError') {
-      return new AccessError({ message: 'С токеном что-то не так' });
+      return next(new AccessError({ message: 'С токеном что-то не так' }));
     }
   }
   req.user = payload;
